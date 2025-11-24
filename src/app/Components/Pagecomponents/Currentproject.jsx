41d70d7cd75d079202f6e5728fc5b48781017d3e
techbase2brand <techbase2brand@gmail.com>
2025-11-24 16:18:20 +0530

    update core investments section phone update

--- src/app/Components/Pagecomponents/Currentproject.jsx
+++ src/app/Components/Pagecomponents/Currentproject.jsx
@@ -57,13 +57,20 @@ const Currentproject = () => {
               {industries.map((data, index) =>
                 <Link key={index} href={data.route}>
                   <div className="p-4 gradient_border group rounded-[50px] hover:shadow-[-4px_4px_4px_#ffffff99] hover:scale-[1.02] transition-all duration-700 relative overflow-hidden magnetic max-w-[450px] xl:max-w-[520px] 2xl:max-w-[550px] m-auto">
-                    
                     {/* Background Gradient */}
                     <div className="absolute inset-0 bg-gradient-to-t from-[#000000] to-[#001D3C] group-hover:opacity-100 transition-opacity duration-500" />
 
                     {/* Floating Particle */}
                     <div className="absolute top-4 right-4 w-2 h-2 bg-primary/40 rounded-full" />
-
+                    {/* <div className="absolute w-[60px] md:w-[64px] h-[60px] md:h-[64px] flex items-center justify-center bg-[var(--secondary)] group-hover:bg-[var(--primary)] rounded-[50%] transition-all duration-500 z-[2]">
+                      <Image
+                        src={data.icon}
+                        alt="icon"
+                        width={1000}
+                        height={500}
+                        className="max-w-[24px] md:max-w-[34px]"
+                      />
+                    </div> */}
                     <div className="relative max-w-[510px] m-auto cursor-ripple">
                       <div>
                         <Image
@@ -71,10 +78,10 @@ const Currentproject = () => {
                           alt="banner"
                           width={1000}
                           height={500}
-                          className="rounded-lg"
+                          className="md:rounded-lg min-h-[299px] md:min-h-auto object-cover rounded-[35px]"
                         />
                       </div>
-                     
+
                       <div>
                         <Arrowbtn
                           width_height={
